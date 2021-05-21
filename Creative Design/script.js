936b19934a6bd306d518f218c3c247b6cb1704a9
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-05-21 14:05:21 +0530

    JS
    Added JS for Teams Story Button

--- Creative Design/script.js
+++ Creative Design/script.js
@@ -2,6 +2,7 @@ const stories = document.querySelectorAll(".story");
 document.querySelectorAll(".story-btn").forEach((btn, i) => {
   btn.addEventListener("click", () => {
     btn.classList.toggle("change");
-    stories.classList.toggle("change");
+    stories[i].classList.toggle("change");
+    console.log(1);
   });
 });
